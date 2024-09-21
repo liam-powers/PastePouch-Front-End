a16b83529aa9b2eea2fcd6dfa5c38691a33e3f42
liam-powers <95184359+liam-powers@users.noreply.github.com>
2024-09-21 12:28:23 -0500

    basic landing page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let links = {
+		github: 'https://github.com/liam-powers',
+		website: 'https://liampowers.me/'
+	};
+</script>
+
+<h1>Welcome to PastePouch!</h1>
+<p>A single-click paste sharing platform with customizable URLs, syntax highlighting, and more.</p>
+<p>
+	This website is currently under development using Svelte and SvelteKit. Bear with this styling for
+	a moment.
+</p>
+<p>
+	Feel free to check out my <a href={links.github} target="_blank" rel="noopener noreferrer">
+		GitHub
+	</a>
+	or
+	<a href={links.website} target="_blank" rel="noopener noreferrer">Website</a>.
+</p>
